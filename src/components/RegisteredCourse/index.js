40f926ed552a477dfa23cfd69d4722e2ed645662
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-12-22 18:56:16 +0700

    add link my courses to see video

--- src/components/RegisteredCourse/index.js
+++ src/components/RegisteredCourse/index.js
@@ -1,64 +1,72 @@
-import React, { Fragment } from 'react'
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
 import classes from './styles.module.scss';
 
+const RegisteredCourse = ({ courses, title }) => {
+  return (
+    <Fragment>
+      <h3 className={classes.title}>{title}</h3>
+      <section className={classes.courseList}>
+        {courses.map((course) => (
+          <div
+            // onClick={() => getcourseByCategory(category.id, category.category_name)}
+            key={course.id}
+          >
+            <Link
+              to={{
+                pathname: '/video-course',
+                state: {
+                  course_id: course.id,
+                },
+              }}
+            >
+              <div className={classes.courseCard}>
+                <img src={course.img_source} alt="Course" />
+                <div className={classes.content}>
+                  <h6 className={classes.name}>{course.title}</h6>
+                  <p className={classes.lettor}>{course.lecturer}</p>
+                  <ProgressBar progress={course.progress} height={20} />
+                </div>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </section>
+    </Fragment>
+  );
+};
 
-const RegisteredCourse = ({courses, title}) => {
-    return (
-        <Fragment>
-            <h3 className={classes.title}>{title}</h3>
-            <section className={classes.courseList}>
-                {courses.map(course => (
-                    <div 
-                        // onClick={() => getcourseByCategory(category.id, category.category_name)}
-                        key={course.id}
-                    >
-                        <div className={classes.courseCard}>
-                            <img src={course.img_source} alt='Course' /> 
-                            <div className={classes.content}>
-                                <h6 className={classes.name}>{course.title}</h6>
-                                <p className={classes.lettor}>{course.lecturer}</p>
-                                <ProgressBar progress={course.progress} height={20} />
-                            </div>
-                        </div>
-                    </div>
-                ))}
-            </section>
-        </Fragment>
-    );
-}
+const ProgressBar = ({ progress, height }) => {
+  const Parentdiv = {
+    height: height,
+    width: '90%',
+    backgroundColor: 'whitesmoke',
+    borderRadius: 40,
+    margin: 5,
+    border: '1px solid #000000',
+  };
 
-const ProgressBar = ({progress,height}) => {
-     
-    const Parentdiv = {
-        height: height,
-        width: '90%',
-        backgroundColor: 'whitesmoke',
-        borderRadius: 40,
-        margin: 5,
-        border: '1px solid #000000',
-      }
-      
-      const Childdiv = {
-        height: '100%',
-        width: `${progress}%`,
-        backgroundColor: 'black',
-       borderRadius:40,
-        textAlign: 'right'
-      }
-      
-      const progresstext = {
-        padding: 10,
-        color: 'white',
-        fontWeight: 900
-      }
-        
-    return (
+  const Childdiv = {
+    height: '100%',
+    width: `${progress}%`,
+    backgroundColor: 'black',
+    borderRadius: 40,
+    textAlign: 'right',
+  };
+
+  const progresstext = {
+    padding: 10,
+    color: 'white',
+    fontWeight: 900,
+  };
+
+  return (
     <div style={Parentdiv}>
       <div style={Childdiv}>
         <span style={progresstext}>{`${progress}%`}</span>
       </div>
     </div>
-    )
-}
+  );
+};
 
 export default RegisteredCourse;
--- src/pages/VideoCourse/index.js
+++ src/pages/VideoCourse/index.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import { useLocation } from 'react-router-dom';
 import Layout from '../../layout/Layout';
 
 const VideoCourse = () => {
+  const location = useLocation();
+  const stateLocation = location.state;
   return (
     <Layout>
-      <div className='demo-course-video'>Hello world</div>
+      <div className='demo-course-video'>Hello world {stateLocation.course_id}</div>
     </Layout>
   );
 };
