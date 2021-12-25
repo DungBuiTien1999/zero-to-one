81225751f2aa85cf2684779e6522fc39045e1238
DungBuiTien1999 <nguyentonhi199@gmail.com>
2021-12-25 20:36:06 +0700

    demo video screen

--- src/pages/VideoCourse/index.js
+++ src/pages/VideoCourse/index.js
@@ -1,13 +1,73 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
+import HtmlToReact from 'html-to-react';
 import Layout from '../../layout/Layout';
-
+import { myCourses } from '../../assets/data/home';
 const VideoCourse = () => {
   const location = useLocation();
   const stateLocation = location.state;
+
+  const [chapters, setChapters] = useState();
+  const [currentVideo, setCurrentVideo] = useState();
+  const HtmlToReactParser = HtmlToReact.Parser;
+  const htmlToReactParser = new HtmlToReactParser();
+
+  useEffect(() => {
+    setChapters(myCourses[stateLocation.course_id - 1].chapters);
+    setCurrentVideo(
+      myCourses[stateLocation.course_id - 1].chapters[0].videos[0]
+    );
+  }, [stateLocation]);
+
+  const onClickSubVideo = (video) => {
+    setCurrentVideo(video);
+  }
+
   return (
     <Layout>
-      <div className='demo-course-video'>Hello world {stateLocation.course_id}</div>
+      <div className="v-container">
+        {currentVideo && (
+          <section className="v-left">
+            <div>
+              <iframe
+                title={currentVideo.title}
+                className="video-course"
+                src={currentVideo.linkVideo}
+                frameborder="0"
+                allowFullScreen
+                ng-show="showvideo"
+              ></iframe>
+              <h3>{currentVideo.title}</h3>
+              <h3>Description</h3>
+              <div>{htmlToReactParser.parse(currentVideo.description)}</div>
+            </div>
+          </section>
+        )}
+        {chapters && (
+          <div className="v-right">
+            <div className="list-chapter">
+              {chapters.map((chapter) => (
+                <div className="chapter-item">
+                  <p className="chapter-title">{chapter.title}</p>
+                  <div className="chapter-videos">
+                    <ul>
+                      {chapter.videos.map((video) => (
+                        <li onClick={() => onClickSubVideo(video)}>
+                          <img
+                            src={video.thumbail}
+                            alt={video.title}
+                          />
+                          <p>{video.title}</p>
+                        </li>
+                      ))}
+                    </ul>
+                  </div>
+                </div>
+              ))}
+            </div>
+          </div>
+        )}
+      </div>
     </Layout>
   );
 };
